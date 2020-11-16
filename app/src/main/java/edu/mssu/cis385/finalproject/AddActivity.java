9508cb9b32752105ffbf50e626f6740c580d4a22
austinlindsey <austinlindsey2288@gmail.com>
2020-11-15 21:18:38 -0600

    Initial commit

---
+++ app/src/main/java/edu/mssu/cis385/finalproject/AddActivity.java
@@ -0,0 +1,14 @@
+package edu.mssu.cis385.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
---
+++ app/src/main/java/edu/mssu/cis385/finalproject/GroceryDBHelper.java
@@ -0,0 +1,37 @@
+package edu.mssu.cis385.finalproject;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import edu.mssu.cis385.finalproject.GroceryContract.*;
+
+import androidx.annotation.Nullable;
+
+public class GroceryDBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "grocerylist.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public GroceryDBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_GROCERYLIST_TABLE = "Create Table " +
+                GroceryEntry.TABLE_NAME + " (" +
+                GroceryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                GroceryEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+                GroceryEntry.COLUMN_AMOUNT + " INTEGER NOT NULL, " +
+                GroceryEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                ");";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_GROCERYLIST_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + GroceryEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
